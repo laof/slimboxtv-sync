ba255ee51d8e39a017b8ceac1450533b529d42ee
denglan <18030893186@163.com>
2022-10-23 15:42:54 +0800

    ccc

---
+++ scripts/all.js
@@ -0,0 +1,19 @@
+import fs from 'fs'
+import { getData } from '../all.js'
+const today = new Date().toLocaleString('zh-cn', { timeZone: 'Asia/Shanghai' })
+
+let table = [
+  '当前同步于 ' + today,
+  '| 文件名 | 大小 | 发布日期 |',
+  '| ---- | ---- | ---- |'
+]
+
+fs.mkdir('output', (err) => {
+  if (err) {
+    return
+  }
+
+  getData().then((res) => {
+    fs.writeFile('output/all_.json', JSON.stringify(res), () => {})
+  })
+})
