50167994fc9f65663c9ae6e8dec3e79452fcf61f
lovalong <Lova_H_Long@manulife.com>
2022-10-24 22:12:21 +0800

    add

--- scripts/update.js
+++ scripts/update.js
@@ -3,6 +3,24 @@ import { readme, update } from './_helper.js'
 
 const { history, box } = update()
 
-const now = await get(box)
-const data = history.concat(...now)
+const list = []
+
+try {
+  await get(box)
+} catch (e) {
+  console.log('job failed!!!')
+}
+
+box.forEach((item) => {
+  try {
+    const uri = item.disk[0].link[0].files[0].url
+    if (uri.startsWith('https://')) {
+      list.push(item)
+    }
+  } catch (e) {
+    console.log('update failed:' + item.box)
+  }
+})
+
+const data = history.concat(...list)
 readme(data)
