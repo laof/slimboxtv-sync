fe20654546558399223e9a50f80b13ab436c5b49
denglan <18030893186@163.com>
2022-10-29 11:07:04 +0800

    update

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 import { update, box, opts } from './src/helper.js'
 import { updateInfo } from './src/info.js'
 
-await updateInfo(box)
-console.log(box)
+
+
+console.log(opts)
+
+// await updateInfo(box)
