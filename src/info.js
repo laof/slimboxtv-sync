5b9b50f2d7a3a94ca34be3075e85e2f81e8a78cd
denglan <18030893186@163.com>
2022-10-29 11:12:46 +0800

    add

--- src/info.js
+++ src/info.js
@@ -1,9 +1,11 @@
 import { product } from './slimboxtv.js'
 export async function updateInfo(box) {
-  for (let item of box) {
-    const { list } = await product(item.homepage)
+  for (let [index, item] of box.entries()) {
+    console.log(`[${++index}/${box.length}] ${item.box}`)
+    const { error, list } = await product(item.homepage)
     if (list.length) {
       item.disk = list
     }
+    error.forEach((err) => console.log('[error]', err))
   }
 }
