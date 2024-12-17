6f705fe36d4b880859e9dfceec05835d32c85357
laof <345603828@qq.com>
2024-12-17 21:59:19 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// res, err := http.Get("http://192.168.31.150:6200/test.json")
+	res, err := http.Get("https://raw.githubusercontent.com/2hacc/TVBox/main/oktv.json")
+
+	if err != nil {
+		log.Fatalln("http error: " + err.Error())
+		return
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Fatalln("ReadAll error: " + err.Error())
+		return
+	}
+
+	f := "conf/ok.json"
+
+	os.WriteFile(f, body, 0644)
+
+}
